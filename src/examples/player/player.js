74166c4e48288d08bce2b68811f03f4facff552f
Flashphoner <support@flashphoner.com>
2022-02-21 10:15:03 +0700

    SFU Two Way Streaming and Player examples added #WCS-2884

---
+++ src/examples/player/player.js
@@ -0,0 +1,206 @@
+const constants = SFU.constants;
+const sfu = SFU;
+let mainConfig;
+let remoteDisplay;
+let playState;
+const PLAY = "play";
+const STOP = "stop";
+const PRELOADER_URL="../commons/media/silence.mp3"
+
+/**
+ * Default publishing config
+ */
+const defaultConfig = {
+    room: {
+        url: "ws://127.0.0.1:8080",
+        name: "ROOM1",
+        pin: "1234",
+        nickName: "User1"
+    }
+};
+
+/**
+ * Current state object
+ */
+const CurrentState = function(prefix) {
+    let state = {
+        prefix: prefix,
+        pc: null,
+        session: null,
+        room: null,
+        set: function(pc, session, room) {
+            state.pc = pc;
+            state.session = session;
+            state.room = room;
+        },
+        clear: function() {
+            state.room = null;
+            state.session = null;
+            state.pc = null;
+        },
+        buttonId: function() {
+            return state.prefix + "Btn";
+        },
+        buttonText: function() {
+            return (state.prefix.charAt(0).toUpperCase() + state.prefix.slice(1));
+        },
+        inputId: function() {
+            return state.prefix + "Name";
+        },
+        statusId: function() {
+            return state.prefix + "Status";
+        },
+        formId: function() {
+            return state.prefix + "Form";
+        },
+        errInfoId: function() {
+            return state.prefix + "ErrorInfo";
+        },
+        is: function(value) {
+            return (prefix === value);
+        }
+    };
+    return state;
+}
+
+/**
+ * load config and set default values
+ */
+const init = function() {
+    let configName = getUrlParam("config") || "./config.json";
+    $("#playBtn").prop('disabled', true);
+    $("#url").prop('disabled', true);
+    $("#roomName").prop('disabled', true);
+    $("#playName").prop('disabled', true);
+    playState = CurrentState(PLAY);
+    $.getJSON(configName, function(cfg){
+        mainConfig = cfg;
+        onDisconnected(playState);
+    }).fail(function(e){
+        //use default config
+        console.error("Error reading configuration file " + configName + ": " + e.status + " " + e.statusText)
+        console.log("Default config will be used");
+        mainConfig = defaultConfig;
+        onDisconnected(playState);
+    });
+    $("#url").val(setURL());
+    $("#roomName").val("ROOM1-"+createUUID(4));
+    $("#playName").val("Player1-"+createUUID(4));
+}
+
+/**
+ * connect to server
+ */
+const connect = function(state) {
+    //create peer connection
+    pc = new RTCPeerConnection();
+    //get config object for room creation
+    const roomConfig = getRoomConfig(mainConfig);
+    roomConfig.pc = pc;
+    roomConfig.url = $("#url").val();
+    roomConfig.roomName = $("#roomName").val();
+    roomConfig.nickname = $("#" + state.inputId()).val();
+    // connect to server and create a room if not
+    const session = sfu.createRoom(roomConfig);
+    session.on(constants.SFU_EVENT.CONNECTED, function(room) {
+        state.set(pc, session, room);
+        onConnected(state);
+        setStatus(state.statusId(), "ESTABLISHED", "green");
+    }).on(constants.SFU_EVENT.DISCONNECTED, function() {
+        state.clear();
+        onDisconnected(state);
+        setStatus(state.statusId(), "DISCONNECTED", "green");
+    }).on(constants.SFU_EVENT.FAILED, function(e) {
+        state.clear();
+        onDisconnected(state);
+        setStatus(state.statusId(), "FAILED", "red");
+        setStatus(state.errInfoId(), e.status + " " + e.statusText, "red");
+    });
+}
+
+const onConnected = function(state) {
+    $("#" + state.buttonId()).text("Stop").off('click').click(function () {
+        onStopClick(state);
+    }).prop('disabled', false);
+    $('#url').prop('disabled', true);
+    $("#roomName").prop('disabled', true);
+    $("#" + state.inputId()).prop('disabled', true);
+    // Add errors displaying
+    state.room.on(constants.SFU_ROOM_EVENT.FAILED, function(e) {
+        setStatus(state.errInfoId(), e, "red");
+    }).on(constants.SFU_ROOM_EVENT.OPERATION_FAILED, function (e) {
+        setStatus(state.errInfoId(), e.operation + " failed: " + e.error, "red");
+    });
+    playStreams(state);
+}
+
+const onDisconnected = function(state) {
+    $("#" + state.buttonId()).text(state.buttonText()).off('click').click(function () {
+        onStartClick(state);
+    }).prop('disabled', false);
+    $('#url').prop('disabled', false);
+    $("#roomName").prop('disabled', false);
+    $("#" + state.inputId()).prop('disabled', false);
+}
+
+const onStartClick = function(state) {
+    if (validateForm("connectionForm") && validateForm(state.formId())) {
+        $("#" + state.buttonId()).prop('disabled', true);
+        if (state.is(PLAY) && Browser().isSafariWebRTC()) {
+            playFirstSound(document.getElementById("main"), PRELOADER_URL).then(function () {
+                connect(state);
+            });
+        } else {
+            connect(state);
+        }
+    }
+}
+
+const onStopClick = function(state) {
+    $("#" + state.buttonId()).prop('disabled', true);
+    stopStreams(state);
+    state.session.disconnect();
+}
+
+const playStreams = function(state) {
+    //create remote display item to show remote streams
+    remoteDisplay = initRemoteDisplay(document.getElementById("remoteVideo"), state.room, state.pc);
+    state.room.join();
+}
+
+const stopStreams = function(state) {
+    if (remoteDisplay) {
+        remoteDisplay.stop();
+    }
+}
+
+const setStatus = function (status, text, color) {
+    const errField = document.getElementById(status);
+    errField.style.color = color;
+    errField.innerText = text;
+}
+
+const validateForm = function (formId) {
+    var valid = true;
+    $('#' + formId + ' :text').each(function () {
+        if (!$(this).val()) {
+            highlightInput($(this));
+            valid = false;
+        } else {
+            removeHighlight($(this));
+        }
+    });
+    return valid;
+
+    function highlightInput(input) {
+        input.closest('.input-group').addClass("has-error");
+    }
+
+    function removeHighlight(input) {
+        input.closest('.input-group').removeClass("has-error");
+    }
+}
+
+const buttonText = function (string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
