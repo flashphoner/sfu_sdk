7b502794db0184b29431f0019d1dcc64cf042cb3
Flashphoner <support@flashphoner.com>
2024-05-22 10:15:02 +0700

    Bitrate test example refactoring to simplify code #WCS-4133

--- examples/src/bitrate_test/bitrate_test.js
+++ examples/src/bitrate_test/bitrate_test.js
@@ -1,9 +1,9 @@
 const constants = SFU.constants;
 const sfu = SFU;
-let mainConfig;
-let publishState;
+let bitrateTestState;
 
-const PUBLISH = "publish";
+const BITRATE_TEST = "bitrateTest";
+const TEST_DURATION = 30000;
 
 /**
  * Default publishing config
@@ -28,81 +28,41 @@ const CurrentState = function (prefix) {
         pc: null,
         session: null,
         room: null,
-        display: null,
-        roomEnded: false,
-        starting: false,
         bitrateController: null,
         set: function (pc, session, room) {
             state.pc = pc;
             state.session = session;
             state.room = room;
-            state.roomEnded = false;
         },
         clear: function () {
             state.room = null;
             state.session = null;
             state.pc = null;
-            state.roomEnded = false;
+            state.bitrateController = null;
         },
-        setRoomEnded: function () {
-            state.roomEnded = true;
+        durationId: function () {
+            return state.prefix + "Duration";
         },
         buttonId: function () {
             return state.prefix + "Btn";
         },
-        buttonText: function () {
-            return "Connect";
-        },
-        inputId: function () {
-            return state.prefix + "Name";
-        },
         statusId: function () {
             return state.prefix + "Status";
-        },
-        formId: function () {
-            return state.prefix + "Form";
         },
         errInfoId: function () {
             return state.prefix + "ErrorInfo";
         },
-        bitrateButtonId: function () {
-            return state.prefix + "BitrateBtn";
-        },
-        bitrateStatusId: function () {
-            return state.prefix + "BitrateStatus";
+        currentStateId: function () {
+            return state.prefix + "CurrentState";
         },
         getBitrateController: function () {
-            return this.bitrateController;
+            return state.bitrateController;
         },
         setBitrateController: function (controller) {
-            this.bitrateController = controller;
-        },
-        is: function (value) {
-            return (prefix === value);
-        },
-        isActive: function () {
-            return (state.room && !state.roomEnded && state.pc);
+            state.bitrateController = controller;
         },
         isConnected: function () {
             return (state.session && state.session.state() === constants.SFU_STATE.CONNECTED);
-        },
-        isRoomEnded: function () {
-            return state.roomEnded;
-        },
-        setStarting: function (value) {
-            state.starting = value;
-        },
-        isStarting: function () {
-            return state.starting;
-        },
-        setDisplay: function (display) {
-            state.display = display;
-        },
-        disposeDisplay: function () {
-            if (state.display) {
-                state.display.stop();
-                state.display = null;
-            }
         }
     };
     return state;
@@ -112,17 +72,12 @@ const CurrentState = function (prefix) {
  * load config and set default values
  */
 const init = function () {
-    $("#publishBtn").prop('disabled', true);
+    bitrateTestState = CurrentState(BITRATE_TEST);
+    $("#" + bitrateTestState.buttonId()).prop('disabled', true);
     $("#url").prop('disabled', true);
-    $("#roomName").prop('disabled', true);
-    $("#publishName").prop('disabled', true);
-    $("#BitrateBtn").prop('disabled', true);
-    publishState = CurrentState(PUBLISH);
-    mainConfig = defaultConfig;
-    onDisconnected(publishState);
-    $("#url").val(mainConfig.room.url);
-    $("#roomName").val("ROOM1-" + createUUID(4));
-    $("#publishName").val("Publisher1-" + createUUID(4));
+    onDisconnected(bitrateTestState);
+    $("#url").val(setURL());
+    $("#" + bitrateTestState.durationId()).val(TEST_DURATION);
 }
 
 /**
@@ -132,13 +87,15 @@ const connect = async function (state) {
     //create peer connection
     const pc = new RTCPeerConnection();
     //get config object for room creation
-    const roomConfig = getRoomConfig(mainConfig);
+    const roomConfig = getRoomConfig(defaultConfig);
     roomConfig.url = $("#url").val();
-    roomConfig.roomName = $("#roomName").val();
-    roomConfig.nickname = createUUID(5);
-    // clean state display items
-    setStatus(state.statusId(), "");
-    setStatus(state.errInfoId(), "");
+    roomConfig.roomName = "ROOM1-" + createUUID(4);
+    roomConfig.nickname = "User1" + createUUID(4);
+    // clean status display items
+    setStatus(state.statusId(), " ");
+    setStatus(state.errInfoId(), " ");
+    // clean bitrate display item
+    $("#" + state.currentStateId()).val("");
     // connect to server and create a room if not
     try {
         const session = await sfu.createRoom(roomConfig);
@@ -169,97 +126,78 @@ const connect = async function (state) {
 
 const onConnected = function (state, pc, session) {
     state.set(pc, session, session.room());
-    $("#" + state.buttonId()).text("Disconnect").off('click').click(function () {
+    $("#" + state.buttonId()).text("Stop").off('click').click(function () {
         onStopClick(state);
-    });
-    $("#" + state.bitrateButtonId()).off('click').click(function () {
-        onStartBitrateClick(state);
-    });
+    }).prop('disabled', false);
 
     $('#url').prop('disabled', true);
-    $("#roomName").prop('disabled', true);
-    $("#" + state.inputId()).prop('disabled', true);
+    $("#" + bitrateTestState.durationId()).prop('disabled', true);
     // Add errors displaying
     state.room.on(constants.SFU_ROOM_EVENT.FAILED, function (e) {
         setStatus(state.errInfoId(), e, "red");
-        state.setRoomEnded();
         onStopClick(state);
     }).on(constants.SFU_ROOM_EVENT.OPERATION_FAILED, function (e) {
         onOperationFailed(state, e);
     }).on(constants.SFU_ROOM_EVENT.ENDED, function () {
         setStatus(state.errInfoId(), "Room " + state.room.name() + " has ended", "red");
-        state.setRoomEnded();
         onStopClick(state);
     }).on(constants.SFU_ROOM_EVENT.DROPPED, function () {
         setStatus(state.errInfoId(), "Dropped from the room " + state.room.name() + " due to network issues", "red");
-        state.setRoomEnded();
         onStopClick(state);
     });
-    startStreaming(state);
+    startBitrateTest(state);
 }
 
 const onDisconnected = function (state) {
     state.clear();
-    $("#" + state.buttonId()).text(state.buttonText()).off('click').click(function () {
+    $("#" + state.buttonId()).text("Start").off('click').click(function () {
         onStartClick(state);
     }).prop('disabled', false);
     $('#url').prop('disabled', false);
-    $("#roomName").prop('disabled', false);
-    $("#" + state.inputId()).prop('disabled', false);
-    $("#" + state.bitrateButtonId()).prop('disabled', true);
+    $("#" + bitrateTestState.durationId()).prop('disabled', false);
 }
 
 const onStartClick = function (state) {
-    if (validateForm("connectionForm", state.errInfoId())
-        && validateForm(state.formId(), state.errInfoId())) {
-        state.setStarting(true);
-        if (!state.is(PUBLISH) && Browser().isSafariWebRTC()) {
-            playFirstSound(document.getElementById("main"), PRELOADER_URL).then(function () {
-                connect(state);
-            });
-        } else {
-            connect(state);
-        }
+    if (validateForm("connectionForm", state.errInfoId())) {
+        $("#" + state.buttonId()).prop('disabled', true);
+        connect(state);
     }
 }
 
-const onStartBitrateClick = async function (state) {
-    if (state.is(PUBLISH) && validateForm("connectionForm", state.errInfoId())
-        && validateForm(state.formId(), state.errInfoId()) &&
-        state.room) {
-        const statusSelector = $("#" + state.bitrateStatusId());
-        statusSelector.attr("style", "display:inline-block;margin-left: 10px");
+const startBitrateTest = async function (state) {
+    if (state.room) {
+        await state.room.join(state.pc, null, {});
+        const stateSelector = $("#" + state.currentStateId());
+        stateSelector.attr("style", "display:inline-block;margin-left: 10px");
         try {
             const bitrateTest = state.room.getBitrateTest();
             state.setBitrateController(bitrateTest);
             bitrateTest.setListener({
-                onStatusUpdate(status) {
-                    statusSelector.text(" = " + status);
+                onStatusUpdate(bitrateKbps) {
+                    stateSelector.text("Current bitrate: " + bitrateKbps + " kbps");
                 }
-            })
-            bitrateTest.test(30_000).then((bitrate) => {
-                statusSelector.text("Test ended, last bitrate - " + bitrate);
+            });
+            bitrateTest.test($("#" + bitrateTestState.durationId()).val()).then((bitrateKbps) => {
+                stateSelector.text("Test is finished, last measured bitrate: " + bitrateKbps + " kbps");
                 state.setBitrateController(null);
-                $("#" + state.bitrateButtonId()).text("Start test").off('click').click(function () {
-                    onStartBitrateClick(state);
-                });
-            })
-            $("#" + state.bitrateButtonId()).text("Stop test").off('click').click(function () {
-                onStopBitrateClick(state);
+                onStopClick(state);
             });
         } catch (e) {
-            setStatus(state.errInfoId(), e);
+            if (e.type === constants.SFU_ROOM_EVENT.OPERATION_FAILED) {
+                onOperationFailed(state, e);
+            } else {
+                console.error("Failed to start bitrate test: " + e);
+                setStatus(state.errInfoId(), e.name, "red");
+                onStopClick(state);
+            }
         }
-
     }
 }
 
-const onStopBitrateClick = function (state) {
-    if (state.is(PUBLISH)) {
-        const controller = state.getBitrateController();
-        if (controller) {
-            controller.stop();
-        }
+const stopBitrateTest = function (state) {
+    const controller = state.getBitrateController();
+    if (controller) {
+        controller.stop();
     }
 }
 
@@ -269,48 +207,15 @@ const onOperationFailed = function (state, event) {
     } else {
         setStatus(state.errInfoId(), event, "red");
     }
-    state.setRoomEnded();
     onStopClick(state);
 }
 
 const onStopClick = async function (state) {
-    state.setStarting(false);
-    disposeStateDisplay(state);
     if (state.isConnected()) {
-        $("#" + state.buttonId()).prop('disabled', true);
-        onStopBitrateClick(state);
+        stopBitrateTest(state);
         await state.session.disconnect();
         onDisconnected(state);
     }
-}
-
-const startStreaming = async function (state) {
-    await publishStreams(state);
-    state.setStarting(false);
-}
-
-const publishStreams = async function (state) {
-    if (state.isConnected()) {
-        try {
-            if (state.isConnected() && state.isActive()) {
-                await state.room.join(state.pc, null, null);
-                $("#" + state.bitrateButtonId()).prop('disabled', false);
-            }
-        } catch (e) {
-            if (e.type === constants.SFU_ROOM_EVENT.OPERATION_FAILED) {
-                onOperationFailed(state, e);
-            } else {
-                console.error("Failed to capture streams: " + e);
-                setStatus(state.errInfoId(), e.name, "red");
-                onStopClick(state);
-            }
-        }
-    }
-}
-
-
-const disposeStateDisplay = function (state) {
-    state.disposeDisplay();
 }
 
 const setStatus = function (status, text, color) {
